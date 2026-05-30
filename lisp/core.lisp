69cd6b62cfe55c15dd8d315312b0aad190934657
Bruno Diaz <brunoleandrodiaz07@gmail.com>
2026-05-30 20:59:20 -0300

    Requerimiento 1: Función transición + función auxiliar para validar entradas

--- lisp/core.lisp
+++ lisp/core.lisp
@@ -1,1 +1,29 @@
-; codigos con los requerimientos en fase 1
+;; codigos con los requerimientos en fase 1
+
+;; ========================================================
+;; FUNCIÓN: transicion
+;; NATURALEZA: Pura. Recibe el estado actual y el siguente, devuelve la accion de cambiar si la transicion es valida.
+;; ESTRATEGIA: seleccion condicional
+;; IMPACTO: no destructiva
+;; ========================================================
+(defun transicion (color-actual cambiar-a)
+    (if (validar-transiciones-p color-actual cambiar-a)
+        (list color-actual  (format nil "cambiar-a-~a" cambiar-a))
+        (list color-actual 'accion-por-defecto)
+    )
+)
+
+;; ========================================================
+;; FUNCIÓN: validar-transiciones-p
+;; NATURALEZA: Pura, recibe eñ estado actual y el nuevo, luego analiza si la transicion es valida o no, devuelve t o nil respectivamente
+;; ESTRATEGIA: Función predicado
+;; IMPACTO: no destructiva
+;; ========================================================
+(defun validar-transiciones-p (color-actual cambiar-a)
+    (cond
+        ((and (equal color-actual 'en-rojo) (equal cambiar-a 'verde)) t)
+        ((and (equal color-actual 'en-verde) (equal cambiar-a 'amarillo)) t)
+        ((and (equal color-actual 'en-amarillo) (equal cambiar-a 'rojo)) t)
+        (t nil)
+    )
+)
